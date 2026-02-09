ba0df026a567e63673f16e89ba3c5a0721d9781b
fatim <fat1322im@gmail.com>
2026-02-09 18:15:18 +0300

    Convert to App

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3e1fa43d6bc3c388639943eb0ea5a7b6
 "downloads/cyber-edu.apk": "7361e60c3989360f6d9fdf758e0c36b5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "bb52de7a571f3012f2a60e90886d8e4f",
+"flutter_bootstrap.js": "6333377497e92c5ac39063b60faba793",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
